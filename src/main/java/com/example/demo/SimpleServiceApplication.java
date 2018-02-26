657c7ed682c23d94c53456d36b1ee6552964a1d1
Siva <sdesaraj@novationco.com>
2018-02-26 11:03:35 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/SimpleServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SimpleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServiceApplication.class, args);
+	}
+}
